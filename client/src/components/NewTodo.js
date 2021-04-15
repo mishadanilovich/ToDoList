eb95f7c0f6d8a2b88522dc2608971287beeeafad
mishadanilovich <danilovich36misha@gmail.com>
2021-04-15 11:06:38 +0300

    The layout of the Todo component has been completed

--- client/src/components/NewTodo.js
+++ client/src/components/NewTodo.js
@@ -1,18 +1,42 @@
 import React from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { fetchTodoList } from '../actions';
 import Todo from './Todo';
 import plus from '../img/plus.svg';
 
 class NewTodo extends React.Component {
+  componentDidMount() {
+    this.props.fetchTodoList();
+  }
+
+  renderTodoList = () => {
+    return this.props.todoList.map(todo => {
+      return (
+        <div key={todo.id}>
+          <Todo initialValues={_.pick(todo, 'title', 'description')} />
+        </div>
+      );
+    });
+  };
+
   render() {
+    if (!this.props.todoList) return <div>Loading...</div>;
+
     return (
       <div>
-        <button className="adding__new-note">
+        <button onClick={this.onAddClick} className="adding__new-note">
           <img src={plus} alt="Plus" className="adding__new-note_icon" />
           Add
         </button>
+        {this.renderTodoList()}
       </div>
     );
   }
 }
 
-export default NewTodo;
+const mapStateToProps = state => {
+  return { todoList: Object.values(state.todoList) };
+};
+
+export default connect(mapStateToProps, { fetchTodoList })(NewTodo);
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,16 +1,57 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import cross from '../img/delete.svg';
+import '../style/Todo.css';
 
-const Todo = props => {
-  return (
-    <div className="adding">
-      <div className="adding__note">
+class Todo extends React.Component {
+  renderInputTitle = ({ input }) => {
+    return (
+      <input {...input} autoComplete="off" className="field field__title" />
+    );
+  };
+
+  renderInputDescrition = ({ input }) => {
+    return (
+      <input
+        {...input}
+        autoComplete="off"
+        className="field field__description"
+      />
+    );
+  };
+
+  renderAction = action => {
+    if (action === 'confirm') {
+      return (
         <button className="adding__confirm">
           <div className="adding__circle"></div>
         </button>
-        <input type="text" />
-      </div>
-    </div>
-  );
-};
+      );
+    }
 
-export default Todo;
+    if (action === 'delete') {
+      return (
+        <button className="adding__delete">
+          <div className="adding__circle">
+            <img src={cross} alt="cross" class="adding__delete-icon" />
+          </div>
+        </button>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <form className="adding">
+        <div className="adding__note">
+          {this.renderAction('confirm')}
+          <Field name="title" component={this.renderInputTitle} />
+          <Field name="description" component={this.renderInputDescrition} />
+          {this.renderAction('delete')}
+        </div>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({ form: 'todoForm' })(Todo);
